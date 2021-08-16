87618486b621455015986583ec6036c2436629cd
Vitor <vitordwb@gmail.com>
2021-08-16 17:46:11 -0300

    exercise 5

---
+++ 02-loops/5.c
@@ -0,0 +1,23 @@
+/*************************************************************************************************
+ * AUTHOR: Vitor Watanabe
+ * VISIT: https://github.com/vitordwb/fei-ccp130
+ * 
+ * A seguinte sequência de números 0, 1, 1, 2, 3, 5, 8, 13, 21, ... é conhecida como série de
+ * Fibonacci. Nesta sequência, cada número, depois dos 2 primeiros, é igual à soma dos 2
+ * anteriores. Escreva um algoritmo que leia um inteiro N (N < 46) e mostre os N primeiros números
+ * dessa série.
+ * Saída: Os valores devem ser mostrados na mesma linha, separados por um espaço em branco. Não deve
+ * haver espaço após o último valor.
+ * 
+ * EXEMPLO:
+ * Entrada: 5
+ * 
+ * Saída: 0 1 1 2 3
+ *************************************************************************************************/
+
+#include <stdio.h>
+
+int main(void) {
+
+  return 0;
+}
