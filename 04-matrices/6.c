d094bc3093648d0b814c1df1effb3086985ea3a3
vitordwb <vitordwb@gmail.com>
2021-09-07 10:29:16 -0300

    exercise 6

--- 04-matrices/6.c
+++ 04-matrices/6.c
@@ -1,57 +1,57 @@
-#include <stdio.h>
-#include <string.h>
+#include<stdio.h>
+int main(){
+    int M[20][20];
+    int m, n, i, j, s1=0, s2=0, s3=0, set;
+    printf("\nEnter the Row and Column sizes of Square Matrix : ");
+    scanf("%d %d", &m, &n);
 
-int main() {
-	int n=4, matiz[n][n];
- 
-    // set all slots as 0
-    memset(matiz, 0, sizeof(matiz));
- 
-    // Initialize position for 1
-    int i = n / 2;
-    int j = n - 1;
- 
-    // One by one put all values in magic square
-    for (int num = 1; num <= n * n;) {
-        if (i == -1 && j == n) // 3rd condition
-        {
-            j = n - 2;
-            i = 0;
+    if(m==n){
+        printf("\n\n Enter the Matrix elements one by one \n");
+            for (i = 1; i<=m; i++)
+                for (j = 1; j<=n; j++)
+                    scanf("%d", &M[i][j]);
+
+        printf("\n The Given Matrix is : \n");
+            for (i = 1; i<=m; i++){
+                for (j = 1; j<=n; j++)
+                    printf(" %d ", M[i][j]);
+                printf("\n");
+            }
+
+        for (i = 1; i <= m; ++i) // For sum of Diagnal
+            for (j = 1; j <= n; ++j)
+                if (i == j)
+                    s1 = s1 + M[i][j];
+            for (i = 1; i<=m; i++) { // Checking for sum of each Rows
+                for (j = 1; j<=n; j++) {
+                    s2 = s2 + M[i][j];
         }
+        if (s1 == s2)
+            set = 1;
         else {
-            // 1st condition helper if next number
-            // goes to out of square's right side
-            if (j == n)
-                j = 0;
- 
-            // 1st condition helper if next number
-            // is goes to out of square's upper side
-            if (i < 0)
-                i = n - 1;
+            set = 0;
+        break;
         }
-        if (matiz[i][j]) // 2nd condition
-        {
-            j -= 2;
-            i++;
-            continue;
+        s2=0;
         }
+
+        for (i = 1; i<=m; i++) { // Checking for sum of each Columns
+            for (j = 1; j<=n; j++) {
+                s3 = s3 + M[j][i];
+        }
+        if (s1 == s3)
+            set = 1;
+        else {
+            set = 0;
+        break;
+        }
+
+        s3=0;
+        }
+        if (set == 1)
+            printf("\n\n The Given Matrix is Magic square");
         else
-            matiz[i][j] = num++; // set number
- 
-        j++;
-        i--; // 1st condition
+            printf("\n\n The Given Matrix is not Magic square");
     }
- 
-    // Print magic square
-    printf("The Magic Square for n=%d:\nSum of "
-           "each row or column %d:\n\n",
-           n, n * (n * n + 1) / 2);
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < n; j++)
-            printf("%3d ", matiz[i][j]);
-        printf("\n");
-    }
-}
-	
-	return 0;
+    return(0);
 }
